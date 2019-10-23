c9c1365966009d8cc91566d21bc679bc197cec7f
Raul Zarate <zarater.raul@gmail.com>
2019-10-23 10:56:39 -0700

    Libraries to do work

---
+++ Gradient.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+#%matplotlib inline
